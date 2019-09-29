0c7b2a8531511284fa1be1a0db51a37e900e113d
tomrennhack <tomrennhack@gmail.com>
2019-09-29 08:28:29 -0400

    added defaults for movie and spotify

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
-
+// ======================================================
 // require the packages
+// ======================================================
+
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
@@ -7,36 +9,44 @@ var axios = require("axios");
 var fs = require("fs");
 var moment = require('moment');
 
+// ======================================================
 // concert this
+// ======================================================
+
 var getMeConcerts = function (bandName) {
+
     axios
     .get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=trilogy")
     .then(function(response) {
+
         for (var i = 0; i < response.data.length; i++) {
-            console.log('--------------'); //  break visually
             console.log("Venue: " + response.data[i].venue.name); // venue name
             console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country); // venue location
             var formattedDate = moment(response.data[i].datetime).format("MM/DD/YYYY"); //  use moment to format dates
             console.log("Date: " + formattedDate); // date
             console.log("Lineup: " + response.data[i].lineup); // lineup
+            console.log('-----------------------------'); //  break visually
         }
     })
     .catch(function(error) {
         if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
         } else if (error.request) {
-        console.log(error.request);
+            console.log(error.request);
         } else {
-        console.log("Error", error.message);
+            console.log("Error", error.message);
         }
         console.log(error.config);
     });
 }
 
+// ======================================================
 // spotify-this-song
-var getArtistNames = function(artist) {
+// ======================================================
+
+var getArtistNames = function (artist) {
     return artist.name; // set variable for artist name
 }
 
@@ -65,12 +75,28 @@ var getMeSpotify = function(songName) {
     });
 }
 
+// ======================================================
+// spotify-this-song NO SONG ENTERED
+// ======================================================
+
+var getMeSpotifyDEFAULT = function() {
+    console.log("You'll need to enter a song name! But here is a SAMPLE of what you would receive: \n-----------------------------");
+    console.log("Artist(s): Ace of Base");
+    console.log("Song Name: The Sign");
+    console.log("Preview Song: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=b3ae88ee2d194583846b52a29e9cafec");
+    console.log("Album: The Sign (US Album) [Remastered]");
+    console.log('-----------------------------');
+}
+
+// ======================================================
 // movie-this
-var getMeMovies = function(movieName) {
+// ======================================================
+
+var getMeMovies = function (movieName) {
+
     axios
     .get("http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy")
     .then(function(response) {
-        console.log('-----------------------------');
         console.log('Title: ' + response.data.Title); // movie title
         console.log('Release Year: ' + response.data.Year); //  release year
         console.log('IMDB rating: ' + response.data.imdbRating); // imdb rating
@@ -79,6 +105,8 @@ var getMeMovies = function(movieName) {
         console.log('Language: ' + response.data.Language); // language
         console.log('Plot: ' + response.data.Plot); // plot
         console.log('Actors: ' + response.data.Actors); // actors
+        console.log('-----------------------------');
+
     })
     .catch(function(error) {
         if (error.response) {
@@ -94,6 +122,45 @@ var getMeMovies = function(movieName) {
     });
 }
 
+// ======================================================
+// movie-this-DEFAULT to Mr. Nobody if no movie entered
+// ======================================================
+
+var getMeMoviesDEFAULT = function() {
+
+    axios
+    .get("http://www.omdbapi.com/?t=mr-nobody&apikey=trilogy")
+    .then(function(response) {
+        console.log("You'll need to enter a moview name! But here is a SAMPLE of what you would receive: \n-----------------------------");
+        console.log('Title: ' + response.data.Title); // movie title
+        console.log('Release Year: ' + response.data.Year); //  release year
+        console.log('IMDB rating: ' + response.data.imdbRating); // imdb rating
+        console.log('Rotten Tomatoes rating: ' + response.data.Ratings[1].Value); //  rotten tomatoes rating
+        console.log('Production Country: ' + response.data.Country); // production country
+        console.log('Language: ' + response.data.Language); // language
+        console.log('Plot: ' + response.data.Plot); // plot
+        console.log('Actors: ' + response.data.Actors); // actors
+        console.log('-----------------------------');
+
+    })
+    .catch(function(error) {
+        if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        } else if (error.request) {
+        console.log(error.request);
+        } else {
+        console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
+}
+
+// ======================================================
+// do-what-it-says reads random.txt for instruction
+// ======================================================
+
 var doWhatItSays = function () {
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) throw err;
@@ -108,16 +175,24 @@ var doWhatItSays = function () {
     })
 }
 
+// ======================================================
+// switch between different commands define above
+// ======================================================
+
 var pick = function (caseData, functionData) {
     switch (caseData) {
         case 'concert-this':
             getMeConcerts(functionData);
             break;
         case 'spotify-this-song':
-            getMeSpotify(functionData);
+            if (functionData) {
+                getMeSpotify(functionData);
+            } else { getMeSpotifyDEFAULT(); }
             break;
         case 'movie-this':
-            getMeMovies(functionData);
+            if (functionData) {
+                getMeMovies(functionData);
+            } else { getMeMoviesDEFAULT(); }
             break;
         case 'do-what-it-says':
             doWhatItSays();
@@ -127,6 +202,9 @@ var pick = function (caseData, functionData) {
     }
 }
 
+// ======================================================
+// define the node arguments in a func and run it
+// ======================================================
 
 var runThis = function (argOne, argTwo) {
     pick (argOne, argTwo);
